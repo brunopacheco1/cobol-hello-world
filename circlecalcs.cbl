c748a53fa5d389a4cb2f7a22f54dc55e5a990b82
Bruno Pacheco <brunopacheco1@yahoo.com>
2020-04-16 22:53:25 +0200

    Adding other verbs and organizing the variables into groups for better understanding.

--- circlecalcs.cbl
+++ circlecalcs.cbl
@@ -8,10 +8,17 @@
        DATA DIVISION.
 
        WORKING-STORAGE SECTION.
-       01  WS-PI            PIC 9(1)V9(5) VALUE 3.14159.
-       01  WS-RADIUS        PIC 9(5)V9(5).
-       01  WS-CIRCUMFERENCE PIC 9(5)V9(5).
-       01  WS-AREA          PIC 9(5)V9(5).
+       01  WS-CONSTANTS.
+           05  WS-PI PIC 9(1)V9(5) VALUE 3.14159.
+
+       01  WS-CIRCLE.
+           05  WS-RADIUS        PIC 9(5)V9(5) VALUE ZEROES.
+           05  WS-CIRCUMFERENCE PIC 9(5)V9(5) VALUE ZEROES.
+           05  WS-AREA          PIC 9(5)V9(5) VALUE ZEROES.
+
+       01  WS-DISPLAY-VALUES.
+           05  WS-DISPLAY-AREA          PIC Z99.99.
+           05  WS-DISPLAY-CIRCUMFERENCE PIC Z99.99.
 
        PROCEDURE DIVISION.
 
@@ -23,19 +30,22 @@
            STOP RUN.
 
        0002-READ-RADIUS.
-           DISPLAY "Insert the circle's radius: ".
+           DISPLAY "Insert the circle radius: ".
            ACCEPT WS-RADIUS.
 
        0003-CALCULATE-CIRCUMFERENCE.
            DISPLAY "Calculating circumference...".
            COMPUTE WS-CIRCUMFERENCE = 2 * WS-RADIUS * WS-PI.
+           MOVE WS-CIRCUMFERENCE TO WS-DISPLAY-CIRCUMFERENCE.
 
        0004-CALCULATE-AREA.
            DISPLAY "Calculating area...".
            COMPUTE WS-AREA = WS-RADIUS ** 2 * WS-PI.
+           MOVE WS-AREA TO WS-DISPLAY-AREA.
 
        0005-PRINT-RESULTS.
-           DISPLAY "The circle's circumference is: ", WS-CIRCUMFERENCE.
-           DISPLAY "The circle's area is: ", WS-AREA.
+           DISPLAY "The circle circumference is: ",
+               WS-DISPLAY-CIRCUMFERENCE.
+           DISPLAY "The circle area is: ", WS-DISPLAY-AREA.
 
        END PROGRAM CIRCLECALCS.
